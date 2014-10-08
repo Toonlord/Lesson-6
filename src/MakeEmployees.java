78fc4b0b1d3b774f2c1c670ef950c83cf47496f1
ryan0861 <ryan0861@ISS147D8-28.scdsb.pri>
2014-10-08 09:04:09 -0400

    Edit

--- src/MakeEmployees.java
+++ src/MakeEmployees.java
@@ -4,8 +4,7 @@ public class MakeEmployees {
     
     public static void main(String[] args) {
         KeyboardReader k = new KeyboardReader();
-        String name =k.readLine("Enter your name: ");
-        System.out.println("Hello " + name);
+        
     }
     
 }
